f86acf1f0c13ebdf365041316a8397352f75962c
joel shum <joel.shumkwoong@gmail.com>
2021-06-10 12:09:29 +0200

    Fonction d'ajout des notes lorsqu'on appuie sur le bouton add note

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const note = document.getElementById('userText')
 const noteContainer = document.getElementById('note_container')
 const buttonAdd = document.getElementById('submit_note')
+const textNoContent = document.getElementById('note_container__noText');
 
-function addNote() {
+function addNote(e) {
+
     const userText = note.value;
-    const childNote = '<div class="note"><h3>Note (index)</h3> <div>' + userText + '</div> <button>view details<button></div>';
-    noteContainer.appendChild += childNote
+    const newNote = document.createElement('div');
+    newNote.classList.add('note')
+    newNote.innerHTML = '<h3>Note (index)</h3> <p class="noteText">' + userText + '</p> <button class="buttonNote">view details</button>';
+
+    const enfant = noteContainer.children;
+
+    function createTextBox () {
+        noteContainer.appendChild(newNote);
+        note.value = "";
+        console.log(newNote)
+    }
+
+    if (enfant[0].classList.contains('delete')){
+        enfant[0].remove(); 
+        createTextBox ();
+    } else {
+        createTextBox ();
+    }
+
+    e.preventDefault();
 }
 
 buttonAdd.addEventListener('click', addNote)
