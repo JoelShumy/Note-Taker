ee87b2533eef35c3e8958120834b04404c82ff96
joel shum <joel.shumkwoong@gmail.com>
2021-06-21 23:01:50 +0200

    ajout des div focus et début de la fonction focus

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const note = document.getElementById('userText')
 const noteContainer = document.getElementById('note_container')
 const buttonAdd = document.getElementById('submit_note')
-const textNoContent = document.getElementById('note_container__noText');
+const textNoContent = document.getElementById('note_container__noText')
+const backgroundFocusNote = document.getElementById('background_note')
+const zoomNote = document.getElementById('zoom_note')
 
 function addNote(e) {
 
     const userText = note.value;
     const newNote = document.createElement('div');
     newNote.classList.add('note')
-    newNote.innerHTML = '<h3>Note (index)</h3> <p class="noteText">' + userText + '</p> <button class="buttonNote">view details</button>';
+    newNote.innerHTML = '<h3>Note (index)</h3> <p class="noteText">' + userText + '</p> <button onclick="focusNote(this)" class="buttonNote">view details</button>';
 
     const enfant = noteContainer.children;
 
@@ -26,6 +28,15 @@ function addNote(e) {
     }
 
     e.preventDefault();
+}
+
+function focusNote(element){
+    const textContainer = element.target;
+    thisText = textContainer.closest("p").value;
+    backgroundFocusNote.classList.remove('invisible');
+    backgroundFocusNote.classList.add('center_note');
+    zoomNote.innerHTML = '<p>' + thisText + '</p>';
+    alert(thisText);
 }
 
 buttonAdd.addEventListener('click', addNote)
